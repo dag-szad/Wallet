2b15eff55a24f6d63954caf5cfe78a699ab51003
Dag <dagmara.szadkowska@wp.pl>
2024-05-20 14:19:10 +0200

    feat: add image component

---
+++ src/assets/components/atoms/LoginRegisterImage/LoginRegisterImage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import loginImage from './login.svg'
+import registerImage from './register.svg'
+import styles from './LoginRegisterImage.module.scss'
+
+const LoginRegisterImage = ({ destination }) => {
+  if (destination === 'login') {
+    return <img src={loginImage} className={styles.image} role="presentation" />
+  } else if (destination === 'register') {
+    return (
+      <img src={registerImage} className={styles.image} role="presentation" />
+    )
+  } else {
+    console.error(
+      'Error: LoginRegisterImage component received an invalid destination prop'
+    )
+    return null
+  }
+}
+
+LoginRegisterImage.propTypes = {
+  destination: PropTypes.oneOf(['login', 'register']).isRequired,
+}
+
+export default LoginRegisterImage

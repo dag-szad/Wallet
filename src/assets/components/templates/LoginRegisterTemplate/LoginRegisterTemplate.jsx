d94ad26176becc5c88c2492881bfb96fff417582
Dag <dagmara.szadkowska@wp.pl>
2024-05-22 13:39:02 +0200

    feat: add props

--- src/assets/components/templates/LoginRegisterTemplate/LoginRegisterTemplate.jsx
+++ src/assets/components/templates/LoginRegisterTemplate/LoginRegisterTemplate.jsx
@@ -2,12 +2,26 @@ import { ImageWithSubtitle } from '../../molecules/ImageWithSubtitle/ImageWithSu
 import { LoginRegisterWidget } from '../../organisms/LoginRegisterWidget.jsx/LoginRegisterWidget.jsx'
 import styles from './LoginRegisterTemplate.module.scss'
 
-const LoginRegisterTemplate = ({ destination }) => {
+const LoginRegisterTemplate = ({
+  destination,
+  handleRegister,
+  handleLogin,
+  setEmail,
+  setPassword,
+  setUsername,
+}) => {
   return (
     <div className={styles.template}>
       <ImageWithSubtitle destination={destination} />
       <div className={styles.template__background}>
-        <LoginRegisterWidget destination={destination} />
+        <LoginRegisterWidget
+          destination={destination}
+          handleRegister={handleRegister}
+          handleLogin={handleLogin}
+          setEmail={setEmail}
+          setPassword={setPassword}
+          setUsername={setUsername}
+        />
       </div>
     </div>
   )

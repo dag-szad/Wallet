33efcf6811234baa2e3feea4aeb58928554ebd29
Dag <dagmara.szadkowska@wp.pl>
2024-05-24 12:26:11 +0200

    feat: update icon component

--- src/assets/components/atoms/Icon/Icon.jsx
+++ src/assets/components/atoms/Icon/Icon.jsx
@@ -45,6 +45,139 @@ const Icon = ({ name, style }) => {
         </svg>
       )
       break
+    case 'bulb':
+      icon = (
+        <svg
+          width="51"
+          height="51"
+          viewBox="0 0 51 51"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={style}
+        >
+          <path
+            d="M19.6021 37.7058H32.1021"
+            stroke="black"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M21.6854 43.9558H30.0188"
+            stroke="black"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M19.6038 31.4558C19.6041 27.2891 18.5624 26.2475 16.4788 24.1641C14.3951 22.0808 13.4025 19.9702 13.3538 16.8725C13.2538 10.5189 17.5201 6.45581 25.8537 6.45581C34.1875 6.45581 38.4537 10.5189 38.3537 16.8725C38.305 19.9702 37.3118 22.0808 35.2287 24.1641C33.1458 26.2475 32.1041 27.2891 32.1037 31.4558"
+            stroke="black"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      )
+      break
+    case 'reports':
+      icon = (
+        <svg
+          width="51"
+          height="51"
+          viewBox="0 0 51 51"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={style}
+        >
+          <path
+            d="M19.2687 43.9558H31.7687M19.2687 43.9558V33.5391M19.2687 43.9558H8.01868C7.32832 43.9558 6.76868 43.3962 6.76868 42.7058V34.7891C6.76868 34.0987 7.32832 33.5391 8.01868 33.5391H19.2687M31.7687 43.9558V18.9558M31.7687 43.9558H43.0187C43.7091 43.9558 44.2687 43.3962 44.2687 42.7058V7.70581C44.2687 7.01546 43.7091 6.45581 43.0187 6.45581H33.0187C32.3283 6.45581 31.7687 7.01546 31.7687 7.70581V18.9558M19.2687 33.5391V20.2058C19.2687 19.5155 19.8283 18.9558 20.5187 18.9558H31.7687"
+            stroke="black"
+            stroke-width="3"
+          />
+        </svg>
+      )
+      break
+    case 'graph':
+      icon = (
+        <svg
+          width="51"
+          height="51"
+          viewBox="0 0 51 51"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={style}
+        >
+          <path
+            d="M41.852 41.8724H8.51868V8.53906"
+            stroke="black"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M8.51868 34.5808L25.1853 18.9558L31.4353 25.2058L40.8103 15.8308"
+            stroke="black"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      )
+      break
+    case 'graph':
+      icon = (
+        <svg
+          width="51"
+          height="51"
+          viewBox="0 0 51 51"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={style}
+        >
+          <path
+            d="M41.852 41.8724H8.51868V8.53906"
+            stroke="black"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M8.51868 34.5808L25.1853 18.9558L31.4353 25.2058L40.8103 15.8308"
+            stroke="black"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      )
+      break
+    case 'clock':
+      icon = (
+        <svg
+          width="51"
+          height="51"
+          viewBox="0 0 51 51"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={style}
+        >
+          <path
+            d="M25.8521 12.7058V25.2058H38.3521"
+            stroke="black"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M25.8521 46.0392C37.3579 46.0392 46.6854 36.7117 46.6854 25.2059C46.6854 13.7 37.3579 4.37256 25.8521 4.37256C14.3461 4.37256 5.01874 13.7 5.01874 25.2059C5.01874 36.7117 14.3461 46.0392 25.8521 46.0392Z"
+            stroke="black"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      )
+      break
     default:
       console.log('Icon component error')
       icon = null

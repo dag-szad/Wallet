350e2726e17e402b8d41d9c0b042e019d87840f7
Dag <dagmara.szadkowska@wp.pl>
2024-05-28 16:13:55 +0200

    feat: update logo component to link main page

--- src/assets/components/atoms/Logo/Logo.jsx
+++ src/assets/components/atoms/Logo/Logo.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom'
 import logo from './logo.svg'
 import styles from './Logo.module.scss'
 
 const Logo = () => {
   return (
-    <div className={styles.logo}>
+    <Link to="/" className={styles.logo}>
       <img src={logo} role="presentation" className={styles.logo__icon} />
       <span className={styles.logo__title}>Wallet</span>
-    </div>
+    </Link>
   )
 }
 

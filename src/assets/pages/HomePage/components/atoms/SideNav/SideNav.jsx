6de6ffe9412e9a0ff5145f45d5b47810a7cb47c7
Dag <dagmara.szadkowska@wp.pl>
2024-05-28 16:14:27 +0200

    feat: change sive nav display

--- src/assets/pages/HomePage/components/atoms/SideNav/SideNav.jsx
+++ src/assets/pages/HomePage/components/atoms/SideNav/SideNav.jsx
@@ -1,35 +1,61 @@
-import { Icon } from '../../../../../components/atoms/Icon/Icon'
-import { Logo } from '../../../../../components/atoms/Logo/Logo'
+import { Link } from 'react-router-dom'
+
+import { Icon } from '../../../../../components/atoms/Icon/Icon.jsx'
+import { Logo } from '../../../../../components/atoms/Logo/Logo.jsx'
+import { BasicButtons } from '../../../../../components/atoms/BasicButtons/BasicButtons.jsx'
+
 import styles from './SideNav.module.scss'
 
-const SideNav = () => {
-  const classes = `${styles.side} ${styles.isHidden}`
+const SideNav = ({ visible, setNavVisible }) => {
+  const classes =
+    visible !== true ? `${styles.side} ${styles.isHidden}` : `${styles.side}`
 
   return (
     <div className={classes}>
-      <Icon name="cancel" style={styles.side__icon} />
+      <div onClick={setNavVisible}>
+        <Icon name="cancel" style={styles.side__icon} />
+      </div>
+
       <nav className={styles.side__nav}>
         <ul className={styles.nav}>
-          <li>
+          <li onClick={setNavVisible}>
             <Logo />
           </li>
-          <li>
+          <li onClick={setNavVisible}>
             <a href="#about" className={styles.nav__link}>
               about
             </a>
           </li>
-          <li>
+          <li onClick={setNavVisible}>
             <a href="#features" className={styles.nav__link}>
               features
             </a>
           </li>
-          <li>
+          <li onClick={setNavVisible}>
             <a href="#description" className={styles.nav__link}>
               how it works
             </a>
           </li>
         </ul>
       </nav>
+      <div className={styles.side__buttons}>
+        <Link to="/login">
+          <BasicButtons
+            type="button"
+            name="Log in"
+            size="medium"
+            colorScheme="white"
+          />
+        </Link>
+        <Link to="/register">
+          <BasicButtons
+            type="button"
+            name="Sign up"
+            size="medium"
+            colorScheme="green"
+          />
+        </Link>
+      </div>
     </div>
   )
 }
